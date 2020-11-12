cce5a12b161feabb636e51fdcbe69d3eb8f379cf
chem.chikweze <53623147+chem-chikweze@users.noreply.github.com>
2020-11-12 17:29:15 -0500

    csg hash function built untested

--- relations/csg.c
+++ relations/csg.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define courseSize 5
 #define gradeSize 2
 #define BASE (256)
 #define m (1008)
-
-
-// // add date. What part should it be? Attribute or key?
-// // const int courseSize = 5;
-// // const int gradeSize = 2;
 
 typedef struct CSG *CSGLIST;
 struct CSG {
@@ -17,51 +13,45 @@ struct CSG {
     int StudentId;  //conjugate key
     char *Course;    // = (char*) malloc (courseSize * sizeof(char)); //conjugate key
     char *Grade;     //  = (char*) malloc (gradeSize * sizeof(char)); //conjugate key
-    // char Course[courseSize]; //conjugate key
-    // char Grade[gradeSize];
 };
 typedef CSGLIST HASHTABLE[1009];
 
-// unsigned long hashCode (char *Course, int StudentId) {
-//     unsigned long hash;
-//     unsigned const char *cs;
-//     // elements will be treated as having values greater than 0
-//     cs = (unsigned const char *) Course;
-//     hash = 0;
-//     // loops until the end of chars in Course
-//     while (*cs != '\0')
-//     {
-//         // multiplies current hash by BASE, adding current Course character and StudentId then mod m
-//         hash = (hash * BASE + *cs + StudentId)% m;
-//         cs++;
-//     }
-//     return hash;
-// }
+unsigned long hashCode (char *Course, int StudentId) {
+    unsigned long hash;
+    unsigned const char *cs;
+    // elements will be treated as having values greater than 0
+    cs = (unsigned const char *) Course;
+    hash = 0;
+    // loops until the end of chars in Course
+    while (*cs != '\0'){
+        // multiplies current hash by BASE, adding current Course character and StudentId then mod m
+        hash = (hash * BASE + *cs + StudentId)% m;
+        cs++;
+    }
+    return hash;
+}
 
-// void insert(char *Course, int StudentId, char *Grade){
-//     int hash =  hashCode(Course, StudentId);
-//     struct CSG *item = (struct CSG*) malloc (sizeof(struct CSG));
-    
-//     // for(int i = 0; i < courseSize; i++){
-//     //     item->Course = *Course[i];
-//     // }
-//     // for(int i = 0; i < gradeSize; i++){
-//     //     item->Grade = *Grade[i];
-//     // }
-    
-//     // item->Course = Course;
-//     // item->StudentId = StudentId;
-//     // item->Grade = Grade;
+CSGLIST createTuple(char* Course, char* Grade, int StudentId){
+    CSGLIST tuple = (CSGLIST)malloc(sizeof(CSGLIST));
+    tuple->Course = (char *)malloc(sizeof(char)*courseSize);
+    tuple->Grade = (char *)malloc(sizeof(char)*gradeSize);
+    tuple->StudentId = StudentId;
+    strcpy(tuple->Course, Course);
+    strcpy(tuple->Grade, Grade);
+    return tuple;
+}
 
-//     // int hashIndex = hashCode(Course, StudentId);
+void insert(char *Course, int StudentId, char *Grade){
+    CSGLIST item = createTuple(Course, Grade, StudentId);
+    int hashIndex = hashCode(Course, StudentId);
 
-//     // go to bucket,, walk through bucket for if it contains the struct or not.
-//     // while (HASHTABLE[hashIndex] != NULL && HASHTABLE[hashIndex] )
-//     // {
-//     //     /* code */
-//     // }
+    // go to bucket,, walk through bucket for if it contains the struct or not.
+    while (HASHTABLE[hashIndex])
+    {
+        /* code */
+    }
     
-// }
+}
 
 int main() {
     printf("Hello World");
